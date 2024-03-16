fa370284a20093fc09b66fd339315af2a0f76686
Anish <79855191+Anish7-anish@users.noreply.github.com>
2024-03-15 21:12:36 -0400

    Time: 60 ms (27.90%) | Memory: 16.7 MB (52.34%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -4,35 +4,43 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        a=[]
-        b=[]
-        while(l1):
-            a.append(l1.val)
-            l1=l1.next
-        while(l2):
-            b.append(l2.val)
-            l2=l2.next
-        a=a[::-1]
-        b=b[::-1]
-        s = [str(i) for i in a]
-        res1 = int("".join(s))
-        s = [str(i) for i in b]
-        res2 = int("".join(s))
 
-        print(res1)
-        print(res2)
-        ans=list(str(res1+res2)[::-1])
-        ans=[int(i) for i in ans] 
-        l3=ListNode()
-        head = l3
-        head.val = ans[0]       
-        for i in range(1, len(ans)):
-            node = ListNode()
-            node.val = ans[i]
-            head.next = node
-            head = head.next
-        return l3
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy=ListNode()
+        curr=dummy
+        carry=0
+
+        while l1 or l2 or carry:
+
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            val=v1+v2+carry
+
+            carry = val//10
+            val = val%10
+
+            curr.next=ListNode(val)
+
+            curr=curr.next
+            l1=l1.next if l1 else None
+            l2=l2.next if l2 else None
+
+        return dummy.next
+
+
+
+
+
+
+
+
+        
+
+
+    
+
+
         
 
             
