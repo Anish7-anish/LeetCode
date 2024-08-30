e609cb64260799f4aab6c9e54623a3061607fd97
Anish <79855191+Anish7-anish@users.noreply.github.com>
2024-08-30 18:23:10 -0400

    Time: 49 ms (43.05%) | Memory: 16.7 MB (8.37%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,22 +1,19 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        a={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        sum=0
-        for i in range(len(s)):
-            if i==0:
-                sum+=a[s[i]]
-            elif (s[i]=="V" or s[i]=="X") and s[i-1]=="I":
-                sum = sum + (a[s[i]] - 2)
-            elif (s[i]=="L" or s[i]=="C") and s[i-1]=="X":
-                sum = sum + (a[s[i]] - 20)
-            elif (s[i]=="D" or s[i]=="M") and s[i-1]=="C":
-                sum = sum + (a[s[i]] - 200)
+        dict1={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        sum1=dict1[s[0]]
+        for i in range(1,len(s)):  
+            if (s[i] == 'V' or s[i] == 'X' )and s[i-1]=='I':
+                sum1 = sum1 - dict1[s[i-1]] + (dict1[s[i]] - dict1[s[i-1]])
+                print(sum1)
+            elif (s[i] == 'L' or s[i] == 'C' )and s[i-1]=='X':
+                sum1 = sum1 - dict1[s[i-1]] + (dict1[s[i]] - dict1[s[i-1]])
+                print(sum1)
+            elif (s[i] == 'D' or s[i] == 'M' )and s[i-1]=='C':
+                sum1 = sum1 - dict1[s[i-1]] + (dict1[s[i]] - dict1[s[i-1]])
+                print(sum1)
             else:
-                sum+=a[s[i]]
-        return sum
-            
+                sum1 += dict1[s[i]]
+        return sum1
 
-            
-                
-            
         
