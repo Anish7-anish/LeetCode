97f7d956b57ccc76e5b3c43084341945f3a2af7c
Anish <79855191+Anish7-anish@users.noreply.github.com>
2024-09-03 19:17:20 -0400

    Time: 107 ms (42.67%) | Memory: 17.7 MB (90.21%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -3,19 +3,13 @@ class Solution:
         l=0
         r=len(numbers)-1
 
-        while(r>l):
-            if numbers[r]>0 and numbers[r]>target:
+        while l<r:
+            cur= numbers[l] + numbers[r]
+            if cur == target:
+                return [l+1,r+1]
+            elif cur>target:
                 r-=1
             else:
-                break
-        h={}
-
-        for i in range(len(numbers)):
-            h[numbers[i]] = i
-        print(h)
-        for i in range(len(numbers)):
-            y=target-numbers[i]
-
-            if y in h and h[y]!=i:
-                return [min(i+1,h[y]+1),max(i+1,h[y]+1)]
+                l+=1
+        
 
