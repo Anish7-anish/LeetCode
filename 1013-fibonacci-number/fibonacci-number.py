168f44d7742338ed51c72b2cb1fcccc31be7eeb4
Anish <79855191+Anish7-anish@users.noreply.github.com>
2024-09-17 18:01:40 -0400

    Time: 38 ms (55.06%) | Memory: 16.7 MB (9.67%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -11,18 +11,32 @@ class Solution:
         
         # return f(n)
 
-        dp = [0] * (n+1)
+        # dp = [0] * (n+1)
+
+        # if n == 0:
+        #     return 0
+        # if n == 1:
+        #     return 1
+
+        # dp[0] = 0
+        # dp[1] = 1
+
+        # for i in range(2, n+1):
+        #     dp[i] = dp[i-2] + dp[i-1]
+
+        # return dp[n]
+
 
         if n == 0:
             return 0
         if n == 1:
             return 1
 
-        dp[0] = 0
-        dp[1] = 1
+        prev = 0
+        cur = 1
 
         for i in range(2, n+1):
-            dp[i] = dp[i-2] + dp[i-1]
+            prev, cur = cur, cur+prev
 
-        return dp[n]
+        return cur
         
