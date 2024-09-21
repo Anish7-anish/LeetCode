c63761842a8722e9279a3dde9243179625952d7b
Anish <79855191+Anish7-anish@users.noreply.github.com>
2024-09-21 18:18:06 -0400

    Time: 42 ms (67.18%) | Memory: 16.9 MB (31.13%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,32 +1,41 @@
 class Solution:
-    def binsearch(self,nums,target,l,r):
-        n=len(nums)
-
-        while l <= r:
-            mid=(l+r)//2
-            if nums[mid]==target:
-                return mid
-            elif target>nums[mid]:
-                l=mid+1
-            else:
-                r=mid-1
-        return -1
     def search(self, nums: List[int], target: int) -> int:
-        n=len(nums)
-        l=0
-        r=n-1
-
-        while l < r:
-            mid=(l+r)//2
+        def bin(nums,target,left,right):
+            while left<=right:
+                mid = (left+right)//2
+                if nums[mid] == target:
+                    return mid
+                elif target > nums[mid]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
             
-            if nums[mid]>nums[r]:
-                l=mid+1
-            else:
-                r=mid
-        print(l,r)
-        if nums[l]<=target and nums[n-1]>=target:
-            return self.binsearch(nums,target,l,n-1)
-        else:
-            return self.binsearch(nums,target,0,r-1)
+            return -1
 
+        if len(nums)==1 and nums[0]==target:
+            return 0
+        elif len(nums)==1 and nums[0]!=target:
+            return -1
+
+        n = len(nums)
+        l=0
+        r = n-1
+
+        while l<r:
+            mid = (l+r)//2
+            if nums[mid]>nums[r]:
+                l = mid+1
+            else:
+                r = mid
+        
+        piv = l
+        print(piv)
+        
+        if target == nums[piv]:
+            return piv
+
+        if target > nums[piv] and target > nums[n-1]:
+            return bin(nums, target, 0, (piv-1))
+        else:
+            return bin(nums, target, piv+1, n-1)
         
