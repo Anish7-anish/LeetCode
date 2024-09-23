9dfb878c53922aef95c2ba3352f9b97934abd302
Anish <79855191+Anish7-anish@users.noreply.github.com>
2024-09-23 18:02:15 -0400

    Time: 33 ms (67.00%) | Memory: 16.6 MB (14.57%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -17,13 +17,29 @@ class Solution:
 
         # return dp[n]
 
-        memo = {1:1, 2:2}
+        # memo = {1:1, 2:2}
 
-        def f(n):
-            if n in memo:
-                return memo[n]
-            else:
-                memo[n] = f(n-2) + f(n-1)
-                return memo[n]
+        # def f(n):
+        #     if n in memo:
+        #         return memo[n]
+        #     else:
+        #         memo[n] = f(n-2) + f(n-1)
+        #         return memo[n]
         
-        return f(n)
+        # return f(n)
+
+        #Bottom up Constant Space
+
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        prev = 1
+        cur = 2
+
+        for i in range(3, n+1):
+            prev, cur = cur, prev+cur
+
+        return cur
+        
