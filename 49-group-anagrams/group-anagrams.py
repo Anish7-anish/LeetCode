a1dd231ec55e118e8bf11eea6de173bd5ed1410d
Anish <79855191+Anish7-anish@users.noreply.github.com>
2024-09-25 18:04:49 -0400

    Time: 97 ms (28.44%) | Memory: 22.4 MB (9.95%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,19 +1,32 @@
+from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        l=[]
-        for i in strs:
-            arr=[]
-            arr=list(i)
-            arr.sort()
-            l.append("".join(arr))
-        print(l)
+        # l=[]
+        # for i in strs:
+        #     arr=[]
+        #     arr=list(i)
+        #     arr.sort()
+        #     l.append("".join(arr))
+        # print(l)
         
-        ans=[]
-        s=[]
-        for i in range(len(l)):
-            item = [strs[j] for j in range(len(l)) if l[j] == l[i]]
-            if not any(item == elem for elem in s):
-                ans.append(item)
-                s.append(item)
-        return ans
+        # ans=[]
+        # s=[]
+        # for i in range(len(l)):
+        #     item = [strs[j] for j in range(len(l)) if l[j] == l[i]]
+        #     if not any(item == elem for elem in s):
+        #         ans.append(item)
+        #         s.append(item)
+        # return ans
+
+        anagram_dict = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+
+            key = tuple(count)
+            anagram_dict[key].append(s)
+
+        
+        return anagram_dict.values()
 
