fccefd2bb805d70529a65486c178577362b04492
Anish <79855191+Anish7-anish@users.noreply.github.com>
2024-09-30 17:26:00 -0400

    Time: 585 ms (85.99%) | Memory: 20.8 MB (35.46%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -2,35 +2,32 @@ class Solution:
 
     def threeSum(self, nums: List[int]) -> List[List[int]]:
 
-        ans=[]
-        n=len(nums)
+        ans = []
+        n = len(nums)
         nums.sort()
 
-        for i in range(0,len(nums)):
-            if nums[i]>0:
+        for i in range(n):
+            if nums[i] > 0:
                 break
-            if i>0 and nums[i] == nums[i-1]:
+            if i > 0 and nums[i]==nums[i-1]:
                 continue
-            
-            lo,hi=i+1,n-1
 
-            while(lo<hi):
-                summ = nums[i]+nums[lo]+nums[hi]
+            l,h = i+1, n-1
 
+            while l<h:
+                summ = nums[i] + nums[l] + nums[h]
                 if summ == 0:
-                    ans.append([nums[i],nums[lo],nums[hi]])
-                    lo+=1
-                    hi-=1
-                    while lo<hi and nums[lo]==nums[lo-1]:
-                        lo+=1
-                    while lo<hi and nums[hi]==nums[hi+1]:
-                        hi-=1
+                    ans.append([nums[i], nums[l], nums[h]])
+                    l+=1
+                    h-=1
+                
+                    while l<h and nums[l] == nums[l-1]:
+                        l+=1
+                    while l<h and nums[h] == nums[h+1]:
+                        h-=1
+                
                 elif summ > 0:
-                    hi-=1
+                    h-=1
                 else:
-                    lo+=1
+                    l+=1
         return ans
-
-
-        
-        
