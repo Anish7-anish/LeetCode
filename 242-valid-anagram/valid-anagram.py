c20c8887f9c15beac6ff83c89da2722700787641
Anish <79855191+Anish7-anish@users.noreply.github.com>
2025-01-16 15:08:03 -0500

    Time: 16 ms (26.52%) | Memory: 17.8 MB (41.83%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,29 +1,21 @@
-from collections import defaultdict
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s)!=len(t):
             return False
-        sfreq = defaultdict(int)
-        tfreq = defaultdict(int)
-
-        for i in s:
-            if i in sfreq:
-                sfreq[i] += 1
+        freq = {}
+        for i in range(len(s)):
+            if s[i] in freq:
+                freq[s[i]]+=1
             else:
-                sfreq[i] = 1
+                freq[s[i]]=1
         
         for i in t:
-            if i in tfreq:
-                tfreq[i] += 1
+            if i in freq:
+                freq[i]-=1
             else:
-                tfreq[i] = 1
-
-        print(sfreq)
-        print(tfreq)
-
-        for i in s:
-            if sfreq[i]!=tfreq[i]:
                 return False
-        
+        for i in freq:
+            if freq[i]>0:
+                return False
         return True
-
+        
