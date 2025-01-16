1384fa702b1b16a39df7d0f6167712d90f76366c
Anish <79855191+Anish7-anish@users.noreply.github.com>
2025-01-16 14:55:18 -0500

    Time: 35 ms (9.65%) | Memory: 17.9 MB (28.88%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,38 +1,22 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        counter = {}
-        sounter={}
-
-        for i in magazine:
-            if i in counter:
-                counter[i] += 1
+        freq = {}
+        for i in range(len(ransomNote)):
+            if ransomNote[i] in freq:
+                freq[ransomNote[i]]+=1
             else:
-                counter[i] = 1
-
-        for j in ransomNote:
-            if j in sounter:
-                sounter[j] += 1
+                freq[ransomNote[i]]=1
+        freq_2 = {}
+        for i in range(len(magazine)):
+            if magazine[i] in freq_2:
+                freq_2[magazine[i]]+=1
             else:
-                sounter[j] = 1
+                freq_2[magazine[i]]=1
 
-        print(counter)
-        print(sounter)
-        # flag=0
-        # for i in sounter:
-        #     if i in counter and sounter[i] <= counter[i]:
-        #         flag=1
-        #     else:
-        #         return False
-        # if flag==1:
-        #     return True
-        for i in ransomNote:
-            if i not in counter:
-                return False
-            elif counter[i] == 1:
-                del counter[i]
-            else:
-                counter[i]-= 1
-        return True
-
-
+        print(freq)
+        print(freq_2)
         
+        for key,val in freq.items():
+            if key not in freq_2 or freq_2[key]<val:
+                return False
+        return True
