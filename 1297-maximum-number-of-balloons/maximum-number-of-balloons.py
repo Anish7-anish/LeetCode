8e6b82163dc2a859e91122b1ee52c834442d8ccc
Anish <79855191+Anish7-anish@users.noreply.github.com>
2025-01-16 15:23:44 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (13.49%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,56 +1,14 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        ballon={"b":1,"a":1,"l":2,"o":2,"n":1}
-        count=0
         counter = defaultdict(int)
-        dict1={}
-        a="ballon"
-
+        balloon = "balloon"
         
-
-        for i in text:
-            if i in ballon:
-                counter[i]+= 1 
-        print(counter)
-
-        if any(c not in counter for c in a):
+        for c in text:
+            if c in balloon:
+                counter[c] += 1
+        
+        if any(c not in counter for c in balloon):
             return 0
-
-        for i in counter:
-            if i in ballon:
-                dict1[i] = counter[i]
-        print("dict1 before normalising",dict1)
-
-        for i in dict1:
-            if i == 'l' or i == 'o':
-                dict1[i] = dict1[i]//2
-        print("after",dict1)
-
-        min1=float('inf')
-        for i in dict1:
-            
-            if dict1[i]<min1:
-                print("hi")
-                min1=dict1[i]
-        return min1
-        
-
-
-        # for i in text:
-        #     print(i)
-        #     print("len",ballon)
-        #     if len(ballon)==0:
-        #         ballon = {"b":1,"a":1,"l":2,"o":2,"n":1}
-        #         count+=1
-        #     if i in ballon and ballon[i] == 1:
-        #         del ballon[i]
-        #         print("del",ballon)
-        #     elif i in ballon and ballon[i]!=0:
-        #         ballon[i]-=1
-        #         print("val", ballon)
-        # if len(ballon)==0:
-        #         ballon = {"b":1,"a":1,"l":2,"o":2,"n":1}
-        #         count+=1
-        # return count
-        
+        else:
+            return min(counter["b"], counter["a"], counter["l"] // 2, counter["o"] // 2, counter["n"])
         
