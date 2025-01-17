2564491d22bb000d4fba9eb8c39700cde22fc3a9
Anish <79855191+Anish7-anish@users.noreply.github.com>
2025-01-17 12:07:17 -0500

    Time: 21 ms (24.48%) | Memory: 22.6 MB (18.66%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,32 +1,22 @@
-from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # l=[]
-        # for i in strs:
-        #     arr=[]
-        #     arr=list(i)
-        #     arr.sort()
-        #     l.append("".join(arr))
-        # print(l)
-        
-        # ans=[]
-        # s=[]
-        # for i in range(len(l)):
-        #     item = [strs[j] for j in range(len(l)) if l[j] == l[i]]
-        #     if not any(item == elem for elem in s):
-        #         ans.append(item)
-        #         s.append(item)
-        # return ans
+        anagram_dict = {}
 
-        anagram_dict = defaultdict(list)
         for s in strs:
-            count = [0] * 26
+            count = [0]*26
             for c in s:
-                count[ord(c) - ord('a')] += 1
-
+                count[ord(c)-ord('a')]+=1
             key = tuple(count)
-            anagram_dict[key].append(s)
-
+            if key in anagram_dict:
+                anagram_dict[key].append(s)
+            else:
+                anagram_dict[key] = [s]
         
-        return anagram_dict.values()
+        l = []
+        for i in anagram_dict.values():
+            l.append(i)
+        return l
 
+
+            
+        
