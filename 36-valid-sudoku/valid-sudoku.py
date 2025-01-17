3bcbc4b2b5001efc16cc16d9f48873981cf01c5a
Anish <79855191+Anish7-anish@users.noreply.github.com>
2025-01-17 11:06:54 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (39.03%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,7 +1,7 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
-        # Validate Rows:
+        # Validate rows:
         for i in range(9):
             s = set()
             for j in range(9):
@@ -11,7 +11,8 @@ class Solution:
                 elif item != ".":
                     s.add(item)
         
-        #Validate Columns
+
+        # Validate Columns:
         for i in range(9):
             s = set()
             for j in range(9):
@@ -20,19 +21,21 @@ class Solution:
                     return False
                 elif item != ".":
                     s.add(item)
+        
+        # Validate Boxes
 
         starts = [(0,0),(0,3),(0,6),
                   (3,0),(3,3),(3,6),
                   (6,0),(6,3),(6,6)]
 
-        for i, j in starts:
+        for i,j in starts:
             s = set()
             for row in range(i,i+3):
-                for col in range(j, j+3):
+                for col in range(j,j+3):
                     item = board[row][col]
                     if item in s:
                         return False
                     elif item != ".":
                         s.add(item)
-        
+
         return True
