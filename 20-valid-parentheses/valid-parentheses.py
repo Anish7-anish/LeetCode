809be6b2b67621f39aa467d2ecc9210e87cbf32c
Anish <79855191+Anish7-anish@users.noreply.github.com>
2025-01-18 12:54:16 -0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (5.76%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,34 +1,27 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stk = []
-        a = list(s)
-        n = len(s)
-        i = 0
 
-        if len(s) == 1:
+        for i in s:
+            if i == "(" or i == "[" or i == "{":
+                stk.append(i)
+            elif i == "}":
+                if stk and stk[-1]=="{":
+                    stk.pop()
+                else:
+                    return False
+            elif i == "]":
+                if stk and stk[-1]=="[":
+                    stk.pop()
+                else:
+                    return False
+            elif i == ")":
+                if stk and stk[-1]=="(":
+                    stk.pop()
+                else:
+                    return False
+        if stk:
             return False
-        while i < n:
-            if a[i] == "(" or a[i] == "{" or a[i] == "[":
-                stk.append(a[i])
-            elif a[i] == "}" and len(stk)>0:
-                if stk[-1] == "{":
-                    stk.pop()
-                else:
-                    return False
-            elif a[i] == "]" and len(stk)>0:
-                if stk[-1] == "[":
-                    stk.pop()
-                else:
-                    return False
-            elif a[i] == ")" and len(stk)>0:
-                if stk[-1] == "(":
-                    stk.pop()
-                else:
-                    return False
-            else:
-                return False
-            i = i+1
+        return True
 
-        if len(stk) == 0:
-            return True
-        return False
+        
