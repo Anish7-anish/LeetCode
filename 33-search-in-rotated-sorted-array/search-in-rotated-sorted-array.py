a664988c7933e85ff8f566dad8b6a4863e069fae
Anish <79855191+Anish7-anish@users.noreply.github.com>
2025-01-20 11:03:33 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (53.84%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,41 +1,44 @@
-class Solution:
+class Solution:    
     def search(self, nums: List[int], target: int) -> int:
-        def bin(nums,target,left,right):
-            while left<=right:
-                mid = (left+right)//2
-                if nums[mid] == target:
+        def binary(arr,target,l,r):
+            while l<=r:
+                mid = (l+r)//2
+                if arr[mid]==target:
                     return mid
-                elif target > nums[mid]:
-                    left = mid + 1
+                elif arr[mid]>target:
+                    r=mid-1
                 else:
-                    right = mid - 1
-            
+                    l=mid+1
             return -1
-
-        if len(nums)==1 and nums[0]==target:
-            return 0
-        elif len(nums)==1 and nums[0]!=target:
-            return -1
+        
+        # if len(nums)==1 and nums[0]==target:
+        #     return 0
+        # elif len(nums)==1 and nums[0]!=target:
+        #     return -1
 
         n = len(nums)
-        l=0
+        l = 0
         r = n-1
 
-        while l<r:
+        while l < r:
             mid = (l+r)//2
             if nums[mid]>nums[r]:
                 l = mid+1
             else:
                 r = mid
         
+        
+        
         piv = l
-        print(piv)
         
         if target == nums[piv]:
             return piv
 
         if target > nums[piv] and target > nums[n-1]:
-            return bin(nums, target, 0, (piv-1))
+            return binary(nums, target, 0, (piv-1))
         else:
-            return bin(nums, target, piv+1, n-1)
+            return binary(nums, target, piv+1, n-1)
+        
+        
+
         
