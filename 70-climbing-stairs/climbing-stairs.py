b1c8af3235a8cfdfe52def0a5cc7e0a2b5a94be9
Anish <79855191+Anish7-anish@users.noreply.github.com>
2025-02-07 10:50:00 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (42.28%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,45 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # if n == 1:
-        #     return 1
-        # if n == 2:
-        #     return 2
-
-        # dp = [0]*(n+1)
-
-        # dp[1] = 1
-        # dp[2] = 2
-
-        # for i in range(3, n+1):
-        #     dp[i] = dp[i-2]+dp[i-1]
-
-        # print(dp)
-
-        # return dp[n]
-
-        # memo = {1:1, 2:2}
-
-        # def f(n):
-        #     if n in memo:
-        #         return memo[n]
-        #     else:
-        #         memo[n] = f(n-2) + f(n-1)
-        #         return memo[n]
+        memo = {1:1,2:2}
         
-        # return f(n)
+        def stairs(x):
+            if x in memo:
+                return memo[x]
+            else:
+                memo[x] = stairs(x-1)+stairs(x-2)
+                return memo[x]
 
-        #Bottom up Constant Space
-
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
-
-        prev = 1
-        cur = 2
-
-        for i in range(3, n+1):
-            prev, cur = cur, prev+cur
-
-        return cur
-        
+        return stairs(n)        
