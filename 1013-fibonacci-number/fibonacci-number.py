6c819a9d7935b00ecf55f4fdcd0e3dd1e533686f
Anish <79855191+Anish7-anish@users.noreply.github.com>
2025-02-07 10:42:31 -0500

    Time: 40 ms (56.46%) | Memory: 18 MB (20.65%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -10,16 +10,33 @@ class Solution:
         # return self.fib(n-1)+self.fib(n-2)
 
         # Top Down DP Memoization
-        memo = {0:0,1:1}
-
-        def f(x):
-            if x in memo:
-                return memo[x]
-            else:
-                memo[x] = f(x-1)+f(x-2)
-                return memo[x]
         
-        return f(n)
+        # memo = {0:0,1:1}
+
+        # def f(x):
+        #     if x in memo:
+        #         return memo[x]
+        #     else:
+        #         memo[x] = f(x-1)+f(x-2)
+        #         return memo[x]
+        
+        # return f(n)
+
+        # Bottom Up Tabulation approach
+
+        if n < 2:
+            return n
+        
+        dp = [0]*(n+1)
+        dp[0] = 0
+        dp[1] = 1
+
+        for i in range(2,n+1):
+            dp[i] = dp[i-1]+dp[i-2]
+        
+        return dp[n]
+        
+
 
 
         
