db6032090dda6971b73b15baf1d50f7aa0e80bc2
Anish <79855191+Anish7-anish@users.noreply.github.com>
2025-02-13 12:32:09 -0500

    Time: 433 ms (5.04%) | Memory: 68.7 MB (13.21%) - LeetSync

---
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.py
@@ -0,0 +1,49 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
+        stk = [root]
+        graph = defaultdict(set)
+        count = 0
+
+        while stk:
+            current_node = stk.pop()
+            if current_node is None:
+                continue
+            count+=1
+            if current_node.left:
+                child = current_node.left
+                graph[current_node.val].add(child.val)
+                graph[child.val].add(current_node.val)
+                stk.append(child)
+            if current_node.right:
+                child = current_node.right
+                graph[current_node.val].add(child.val)
+                graph[child.val].add(current_node.val)
+                stk.append(child)
+        
+        print(graph)
+        
+        num_min = -1
+        que = deque()
+        que.append(start)
+        infected = set()
+        infected.add(start)
+
+        while que:
+            size = len(que)
+            num_min+=1
+            for _ in range(size):
+                curr = que.popleft()
+                for nei_node in graph[curr]:
+                    if nei_node not in infected:
+                        infected.add(nei_node)
+                        que.append(nei_node)
+        return num_min
+
+
+        
