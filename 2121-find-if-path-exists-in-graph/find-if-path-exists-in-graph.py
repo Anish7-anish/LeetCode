8c618375559bbfd816932d77bebdc64fb9059003
Anish <79855191+Anish7-anish@users.noreply.github.com>
2025-05-14 21:46:19 -0400

    Time: 372 ms (67.26%) | Memory: 96.7 MB (60.14%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         d = defaultdict(list)
@@ -5,22 +6,21 @@ class Solution:
         for u,v in edges:
             d[u].append(v)
             d[v].append(u)
-        
-        seen = set()
-        seen.add(source)
-        stk = [source]
 
-        while stk:
-            node = stk.pop()
+        seen = set()
+        stack = []
+        stack.append(source)
+
+        while stack:
+            node = stack.pop()
             if node == destination:
-                print('hi')
                 return True
-            
             for nei_node in d[node]:
                 if nei_node not in seen:
+                    stack.append(nei_node)
                     seen.add(nei_node)
-                    stk.append(nei_node)
-                    
-        return False
         
-        #O(V+E)
+        return False
+
+
+        
