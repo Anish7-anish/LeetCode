19719311695b19b5b1bda4e6a556dea5036dc07d
Anish <79855191+Anish7-anish@users.noreply.github.com>
2025-06-22 11:22:12 -0400

    Time: 9 ms (20.87%) | Memory: 17.6 MB (98.70%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,23 +1,18 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        d = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        i = 0
-        ans = 0
-
-        while(i<len(s)):
-            if i!= len(s)-1 and (s[i] == 'I' and (s[i+1] == 'V' or s[i+1] == 'X')):
-                ans += d[s[i+1]] - d[s[i]]
-                i=i+2
-            elif i!= len(s)-1 and (s[i] == 'X' and (s[i+1] == 'L' or s[i+1] == 'C')):
-                ans += d[s[i+1]] - d[s[i]]
-                i=i+2
-            elif i!= len(s)-1 and (s[i] == 'C' and (s[i+1] == 'D' or s[i+1] == 'M')):
-                ans += d[s[i+1]] - d[s[i]]
-                i=i+2
-            else:
-                ans += d[s[i]]
-                i+=1
-            
-        return ans
+        roman = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        sum1 = 0
 
         
+        
+        for i in range(len(s)):
+            if (s[i] == "V" or s[i] == "X") and i>0 and s[i-1] == "I":
+                sum1+=(roman[s[i]]) - 2*(roman[s[i-1]])
+            elif (s[i] == "L" or s[i] == "C") and i>0 and s[i-1] == "X":
+                sum1+=(roman[s[i]]) - 2*(roman[s[i-1]])
+            elif (s[i] == "D" or s[i] == "M") and i>0 and s[i-1] == "C":
+                sum1+=(roman[s[i]]) - 2*(roman[s[i-1]])
+            else:
+                sum1+=roman[s[i]]
+        
+        return sum1
